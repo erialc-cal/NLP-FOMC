2c5b5ba4c876c4f8bebd7ca42a1343a67358a244
erialc-cal <claire.he@ensae.fr>
2021-11-17 17:09:16 +0000

    preferred rates

--- preferred_rates/scrapping.py
+++ preferred_rates/scrapping.py
@@ -17,7 +17,7 @@ The book gives more information (summary statistics), which we do not need.
 
 
 """
-
+import pandas as pd
 from PyPDF2 import PdfFileReader
 #from PyPDF2 import PdfFileWriter 
 import os
@@ -142,9 +142,14 @@ while idx < len(split_text):
 
 
 #%% ADD CORRESPONDING NAMES TO NUMBER
-import pandas as pd
+
 
 tab = pd.read_excel(project_directory+'/MemberNumbers.xls', header=None, index_col=1).to_dict()[0]
+
+
+
+time = pd.read_excel(project_directory+'/dates.xls')
+
 
 
  #%%    GET RATES
@@ -179,11 +184,7 @@ for i in trange(len(stops)):
             idx+=1
 
             
-       #%% CREATING DATAFRAME
-import pandas as pd  
- 
-df = pd.DataFrame({'rates': rates, 'bias': bias, 'dates':date})
- 
+
     
 #%% USING A COMBINATION TO GET EVERY PAGE
 
@@ -227,7 +228,7 @@ name_list = []
 dates = []
 len_names = []
 
-for i in trange(90):
+for i in trange(88):
     #we have 88 reunions, but 3 documents are empty as they are titles
     try: 
         with open(project_directory+f'/committee_decisions_{i}.txt', 'r') as doc:
@@ -243,49 +244,136 @@ for i in trange(90):
     # get names
     names = []
     rates = []
-    for elem in l:
+    for idx in range(len(l)//2):
+        elem = l[idx]
         if elem in tab.keys():
             names.append(elem)
             name_list.append(elem)
-            
-        
+ 
+            dates.append(date[0])
+    
+    for idx in range(len(l)//2, len(l)):
+        elem = l[idx]
+        if elem in tab.keys():
+            names.append(elem)
+            name_list.append(elem)
+ 
+            dates.append(date[1])       
     try :
-        s1 = l.index(names[0])
-        s2 = l[s1+1:].index(names[0])
-      #  print(s1, s2)
+        s1 = l.index(names[0]) # index du 1er nom 
+        s2 = l[s1+len(names)//2:].index(names[len(names)//2]) # index du 1er nom de la 2e colonne
+        s3 = l.index('Target funds rate') # index du début des statistiques
+        s4 = l[s3+1:].index('Target funds rate') # index du début de la 2nde colonne de statistiques
+
+        
+             
+        ## POSITIONING OF TEXT CHUNKS
+        # 
+        # IF s3-s1 < 0 stats block comes before name block col 1
+        # if s4-s2 < 0 stats block comes before name block col 2
+        
+        # if abs(s3-s1) < 0.2 : name block and stats are one after the other col1
+        # if abs(s4-s2) < 0.2 : name block and stats are one after the other col2
+        
+        
         
     except :
         pass
 
-    if s2/len(l) < 0.75 and s2/len(l) > 0.4 :
-        # get first column stats 
-        start = l.index('Median (all)')+1
-        for elem in l[start:start+len(names)//2]:
-            rates.append(elem)
-        
-        # 13 different statistics we want to skip 
-    
-        # get second column stats
-        # start of second batch of stats Median (all) + nb of names + 12 til end -12
-    
-        for elem in l[start+len(names)+12:-2*12]:
-            rates.append(elem)
-            
-            
-    elif s1/len(l) > 0.20 :
-        # get first column stats 
-        start = 3
-        for elem in l[start+len(names)//2:start+len(names)]:
-            rates.append(elem)
-        
-        # 13 different statistics we want to skip 
-    
-        # get second column stats
-        # start of second batch of stats Median (all) + nb of names + 12 til end -12
-    
-        for elem in l[start+len(names)+len(names)//2+2*12+1:-2*12-2]:
-            rates.append(elem)
 
+    print('\n','col1 stats/name :',(s3-s1)/len(l), 'inter col:',(s2-s1)/len(l), 'col2 stats/name:',(s4-s2)/len(l),'\n')
+
+    if (s2-s1)/len(l) < 0 :
+        print('Second name is in first batch')
+        count = 0
+        while (s2-s1) < 0 and count < len(names)//2:
+            count +=1
+            s2 = l[s1+len(names)//2:].index(names[len(names)//2+count])
+        
+        print('\n','col1 stats/name :',(s3-s1)/len(l), 'inter col:',(s2-s1)/len(l), 'col2 stats/name:',(s4-s2)/len(l),'\n')
+
+    
+    if (s3-s1)/len(l) < 0 : 
+        print("Names' and stats' orders switched in column 1")
+        # Stats are given before names
+        for elem in l[s3+12:s3+12+len(names)//2]:
+            rates.append(elem)
+            
+    elif (s3-s1)/len(l) > 0.25 : 
+    # tous les noms et tous les chiffres avant le paragraphe de stats
+        for elem in l[s1+len(names)//2+1:s1+len(names)]:
+            rates.append(elem)
+        
+    elif (s3-s1)/len(l) < 0.25:
+        for elem in l[s3+12:s3+12+len(names)//2]:
+            rates.append(elem)
+            
+        
+    if (s4-s2)/len(l) < 0.0 :
+        print("Names and stats' orders switched in column 2")
+        # Stats are given before names
+        for elem in l[s4+12:s4+12+len(names)//2]:
+            rates.append(elem)
+     
+    elif (s4-s2)/len(l) < 0.10 :
+        print("Names and stats' orders switched in column 2")
+        # Stats are given before names
+        for elem in l[s4:s4+len(names)//2]:
+            rates.append(elem)
+       
+    elif (s4-s2)/len(l) > 0.25 : 
+    # tous les noms et tous les chiffres avant le paragraphe de stats
+        for elem in l[s2+len(names)//2+1:s2+len(names)]:
+            rates.append(elem)
+        
+    elif (s4-s2)/len(l) < 0.25:
+        for elem in l[s4+12:s4+12+len(names)//2]:
+            rates.append(elem)
+                
+    
+    
+    
+    # elif s2/len(l) < 0.75 and s2/len(l) > 0.4 and s3 > 0.2 and s3 < 0.5 : 
+    #     # all stats after 'Median all'
+    #     # get first column stats 
+    #     start = l.index('Median (all)')+1
+    #     for elem in l[start:start+len(names)//2]:
+    #         rates.append(elem)
+        
+    # #     # 13 different statistics we want to skip 
+    
+    # #     # get second column stats
+    # #     # start of second batch of stats Median (all) + nb of names + 12 til end -12
+    
+    # #     for elem in l[start+len(names)+12:-2*12]:
+    # #         rates.append(elem)
+            
+    # # elif s1 < s3 and s3 < s2 and s4 < s2 and s2/len(l) < 0.6 and s2/len(l) > 0.4:#s2/len(l) < 0.7 and s2/len(l) > 0.4: 
+    # #     #    colonne par colonne, page découpée en 4 colonnes
+    # #     start = len(names)//2+12
+    # #     for elem in l[start:start+len(names)//2]:
+    # #         rates.append(elem)
+    # #     for elem in l[start+len(names)+12:-12]:
+    # #         rates.append(elem)
+    
+    
+    # elif s1/len(l) > 0.20 : 
+    #     #     start of second batch of stats Median (all) + nb of names + 12 til end -12 
+    #     # get first column stats 
+    #     start = l.index('Individual voters’ desired funds rates')+1
+    #     for elem in l[start+len(names)//2:start+len(names)]:
+    #         rates.append(elem)
+        
+    #     # 13 different statistics we want to skip 
+    
+    #     # get second column stats
+       
+    
+    #     for elem in l[start+len(names)+len(names)//2+2*12+1:-2*12-2]:
+    #         rates.append(elem)
+    while len(rates)<len(names):
+        rates.append('0')
+        
     for r in rates :
         if len(r.split()) > 1:
            # print(r.split())
@@ -298,17 +386,72 @@ for i in trange(90):
         preferred.append(rt)
         bias.append(b)
         
+        
     #print(len(rt), len(b), len(names))
     # multiply the date by the number of entries 
-    dates.append(date)
     len_names.append(len(names))
 #### FACING A BIG PROBLEM SCRAPING OF DATA IS NOT COHERENT OF GEOMETRY OF THE TABLES 
 
 
+#%%
+
+df = pd.DataFrame({'preferred':preferred, 'bias':bias, 'names':name_list, 'dates':dates})
+import datetime as dt
+
+df['dates']=df['dates'].astype('datetime64')
+
+#%%
+member_nb, time_list = [],[]
+for name in df.names:
+    member_nb.append(tab[name])
+    
+
+df['member']=member_nb
 
 
+#%%
 
+time = time.set_index('Time')
+m = []
+for month in time.month:
+    m.append(month[::-1][:2][::-1])
+time['fus']=time['YEAR'].astype('str')+'-'+('0'+time['MONTH'].astype('str'))
+list_d = []
+for d in df.dates.astype(str):
+    list_d.append(d[:7])
+    
+df['ym']= list_d
+#%%
+time_list=[]
+l_t = time.fus.tolist()
+for date in df['ym']:
+    try:
+        time_list.append(l_t.index(date))
+    except:
+        time_list.append(np.NaN)
 
+df['Time']=time_list
+
+#%%
+
+import matplotlib.pyplot as plt
+
+def plot_correct_guess_rate(pref):
+    yes = 0
+    no = 0
+    for elem in pref:
+        try:
+            float(elem)
+            yes+=1
+        except:
+            no+=1
+            
+    yes*=1/len(pref)
+    no*= 1/len(pref)
+    plt.figure()
+    plt.bar(['correct', 'incorrect'], [yes, no])
+    plt.title("Percentage of correctedly guessed rates (probably overestimated)")
+    plt.show()
 
 
 
